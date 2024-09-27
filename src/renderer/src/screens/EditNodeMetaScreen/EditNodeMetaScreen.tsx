06343fbc8395fbaaf8c08d2e3cf20c263e32f5aa
Sam Calthrop <sam@calthrop.com>
2024-09-27 10:54:41 +0100

    tracking down bugs

--- src/renderer/src/screens/EditNodeMetaScreen/EditNodeMetaScreen.tsx
+++ src/renderer/src/screens/EditNodeMetaScreen/EditNodeMetaScreen.tsx
@@ -2,23 +2,23 @@ import classes from './EditNodeMetaScreen.module.css';
 import { ScreenWithSidebar } from '../ScreenWithSidebar';
 import { useNavigate } from 'react-router-dom';
 import { Button, Title } from '@mantine/core';
-// import { MDXEditor } from '@mdxeditor/editor';
-// import { headingsPlugin } from '@mdxeditor/editor';
-// import '@mdxeditor/editor/style.css';
-// import { useEffect, useState } from 'react';
+import { MDXEditor } from '@mdxeditor/editor';
+import { headingsPlugin } from '@mdxeditor/editor';
+import '@mdxeditor/editor/style.css';
+import { useEffect, useState } from 'react';
 
 export const EditNodeMetaScreen = (): JSX.Element => {
   const navigate = useNavigate();
-  // const path = './README.md';
+  const path = './README.md';
 
-  // // test
-  // const [fileContents, setFileContents] = useState<string>();
-  // // retrieving the file contents from the backend
-  // useEffect(() => {
-  //   window.ipcAPI.getFileContents(path).then((fileContents) => {
-  //     setFileContents(fileContents);
-  //   });
-  // }, []);
+  // test
+  const [fileContents, setFileContents] = useState<string>();
+  // retrieving the file contents from the backend
+  useEffect(() => {
+    window.ipcAPI.getFileContents(path).then((fileContents) => {
+      setFileContents(fileContents);
+    });
+  }, []);
 
   return (
     <ScreenWithSidebar>
@@ -26,7 +26,7 @@ export const EditNodeMetaScreen = (): JSX.Element => {
         <div className={classes.thing}>
           <Title order={2}>Edit node meta</Title>
 
-          {/* <MDXEditor markdown={fileContents ? fileContents : ''} plugins={[headingsPlugin()]} /> */}
+          <MDXEditor markdown={fileContents ? fileContents : ''} plugins={[headingsPlugin()]} />
 
           <Button variant="subtle" className={classes.button} onClick={() => navigate('/home')}>
             Exit
