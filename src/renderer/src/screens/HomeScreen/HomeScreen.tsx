f6af6c0a04912641951d6ab95f7d7e32e07e4238
Sam Calthrop <sam@calthrop.com>
2024-10-10 22:51:48 +0100

    most of the design writeup

--- src/renderer/src/screens/HomeScreen/HomeScreen.tsx
+++ src/renderer/src/screens/HomeScreen/HomeScreen.tsx
@@ -4,6 +4,8 @@ import { Button, Title, Text } from '@mantine/core';
 import { ScreenWithSidebar } from '../ScreenWithSidebar';
 import { Canvas } from '@renderer/components/Canvas';
 import { drawPulsingDot } from '@renderer/drawing/drawPulsingDot';
+import { Center, SegmentedControl, rem } from '@mantine/core';
+import { IconEye, IconPencil } from '@tabler/icons-react';
 
 export const HomeScreen = (): JSX.Element => {
   const navigate = useNavigate();
@@ -13,6 +15,27 @@ export const HomeScreen = (): JSX.Element => {
       <div className={classes.root}>
         <div className={classes.thing}>
           <Title order={1}>Home</Title>
+          {/* <SegmentedControl
+            data={[
+              {
+                value: 'preview',
+                label: (
+                  <Center style={{ gap: 10 }}>
+                    <IconEye style={{ width: rem(16), height: rem(16) }} />
+                  </Center>
+                ),
+              },
+              {
+                value: 'edit',
+                label: (
+                  <Center style={{ gap: 10 }}>
+                    <IconPencil style={{ width: rem(16), height: rem(16) }} />
+                  </Center>
+                ),
+              },
+            ]}
+            radius="lg"
+          /> */}
           <Canvas drawFunc={drawPulsingDot} width={200} height={200} />
           <Text>Edit node meta</Text>
           <Button
