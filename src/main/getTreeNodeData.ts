aa48697a05607b98ccb99b2553883b842247014a
sc <146133436+samcalthrop@users.noreply.github.com>
2025-03-27 13:46:54 +0000

    adding to writeup

--- src/main/getTreeNodeData.ts
+++ src/main/getTreeNodeData.ts
@@ -1,12 +1,14 @@
 import { TreeNodeData } from '@mantine/core';
 import directoryTree, { DirectoryTree } from 'directory-tree';
 
+// returns a given directory tree in the Array<TreeNodeData> form
 export const convertDirectoryTree = ({ name, path, children }: DirectoryTree): TreeNodeData => ({
   label: name,
   value: path,
   children: children ? children.map(convertDirectoryTree) : undefined,
 });
 
+// returns Array<TreeNodeData> from a given path
 export const getTreeNodeData = (path: string): TreeNodeData[] => {
   const tree = directoryTree(path, { extensions: /\.(md|png|jpg|jpeg|gif)$/ });
   // debug log
